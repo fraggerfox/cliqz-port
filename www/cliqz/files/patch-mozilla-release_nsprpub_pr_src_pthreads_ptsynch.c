fc3bf15b8fa74d5f31d9b42627a0ffaf57adff5b
fraggerfox <santhosh.raju@gmail.com>
2018-11-16 19:40:06 +0530

    Updated the patch file to be consistent with make makepatch output.

--- www/cliqz/files/patch-mozilla-release_nsprpub_pr_src_pthreads_ptsynch.c
+++ www/cliqz/files/patch-mozilla-release_nsprpub_pr_src_pthreads_ptsynch.c
@@ -1,6 +1,6 @@
---- mozilla-release/nsprpub/pr/src/pthreads/ptsynch.c.orig	2018-11-12 14:16:18 UTC
+--- mozilla-release/nsprpub/pr/src/pthreads/ptsynch.c.orig	2018-10-05 16:27:24 UTC
 +++ mozilla-release/nsprpub/pr/src/pthreads/ptsynch.c
-@@ -911,7 +911,8 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphore(const char *
+@@ -911,7 +911,8 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
   * From the semctl(2) man page in glibc 2.0
   */
  #if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) \
