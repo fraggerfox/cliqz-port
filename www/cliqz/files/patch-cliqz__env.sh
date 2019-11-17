a05e6d8a37a634d13b2cbfbc30a7e663d7b47096
fraggerfox <santhosh.raju@gmail.com>
2019-11-17 07:38:45 -0500

    Updates the package from cliqz-1.29.3 to cliqz-1.30.0.

--- www/cliqz/files/patch-cliqz__env.sh
+++ www/cliqz/files/patch-cliqz__env.sh
@@ -16,12 +16,12 @@ phase.
  
  if [ -z $CQZ_BUILD_ID ]; then
    echo "CQZ_BUILD_ID not specified and can not be obtain from "$S3_BUCKET
-@@ -131,7 +131,7 @@ OBJ_DIR=$MOZ_OBJDIR
+@@ -131,6 +131,6 @@ OBJ_DIR=$MOZ_OBJDIR
  SRC_BASE=mozilla-release
  
  # automatic forget tab - start
 -wget -O adult-domains.bin https://s3.amazonaws.com/cdn.cliqz.com/browser-f/APT/adult-domains.bin
 +#wget -O adult-domains.bin https://s3.amazonaws.com/cdn.cliqz.com/browser-f/APT/adult-domains.bin
- export CQZ_AUTO_PRIVATE_TAB=1
  export CQZ_ADULT_DOMAINS_BF=../adult-domains.bin
  # automatic forget tab - end
+
